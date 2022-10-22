96c22dfacee897eb8b9ee84ae8801a45d25269f5
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2022-10-22 20:31:25 +0100

    Add files via upload

--- RatEscape.py
+++ RatEscape.py
@@ -21,18 +21,24 @@ else:
     client_id = "1030917447408164894"  
     RPC = Presence(client_id)  
     RPC.connect()
-    RPC.update(large_image="logo", large_text="RatEscape", state="By unofficialdxnny", details="Main Menu", start=start_time, buttons = [{"label": "Download", "url": "https://github.com/unofficialdxnny/RatEscape/releases/"}, {"label": "Source Code", "url": "https://github.com/unofficialdxnny/RatEscape"}])
+    RPC.update(large_image="logo", large_text="RatEscape", state="By unofficialdxnny", details="Main Menu", start=start_time, buttons = [{"label": "Download", "url": "https://github.com/unofficialdxnny/RatEscape/releases/"}, {"label": "Repository", "url": "https://github.com/unofficialdxnny/RatEscape"}])
+    
+    
+    
+    
     pygame.init()
 
     while True:
         width,height = 1000,500
         screen = pygame.display.set_mode((width, height))
         pygame.display.set_caption('RatEscape')
-        pygame.display.set_icon(pygame.image.load('./Assets/Logo.jpg'))
+        pygame.display.set_icon(pygame.image.load('./Assets/Logo.png'))
         window = pygame.display.set_mode((width,height))
-        bg_img = pygame.image.load('./Assets/Background.jpg')
+        bg_img = pygame.image.load(f'./Assets/RatEscapeBG-{config_data["theme"]}.jpg')
         bg_img = pygame.transform.scale(bg_img,(width,height))
         done = False
+
+
         while not done:
             window.blit(bg_img,(0,0))
             for event in pygame.event.get():
