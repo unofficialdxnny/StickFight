6e777ebc4de9fbcb472a4a1a6d6c40011fad5a9b
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-02-08 18:50:57 +0000

    Added Button and mainmenu

--- Python Edition/StickFight.py
+++ Python Edition/StickFight.py
@@ -4,12 +4,17 @@ from pygame.locals import *
 from pygame import mixer
 import keyboard as kb
 import os
+import msvcrt
+from time import sleep
 
 
 os.system('cls')
 
 
 pygame.display.set_caption('StickFight | Loading')
+
+surface = pygame.display.set_mode((1300, 500))
+
 
 1 == mixer.music.load('BGMPotentialKav.wav')
 mixer.music.play(1)
@@ -18,9 +23,23 @@ pygame.display.set_icon(logo)
 
 
 clip = VideoFileClip('Loading.mp4')
-while True:
-    clip.preview()
 
 
+
+clip.preview()
+
+
+pygame.display.set_caption('StickFight | Main Menu')
+menuColor = (80, 18, 25)
+surface.fill(menuColor)
+playBtn = pygame.image.load('Play.png').convert()
+
+surface.blit(playBtn, (0, 0))
+
+pygame.display.update()
+
+sleep(50)
+
+    
 
 pygame.quit()
