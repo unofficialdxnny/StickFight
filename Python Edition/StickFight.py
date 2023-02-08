37c6ca2d4e17d7a24fa9d8db54185cae805aa8be
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-02-08 13:44:09 +0000

    added music by Kav - Potential

--- Python Edition/StickFight.py
+++ Python Edition/StickFight.py
@@ -1,17 +1,25 @@
 from moviepy.editor import *
 import pygame
 from pygame.locals import *
+from pygame import mixer
 import keyboard as kb
+import os
 
+
+os.system('cls')
 
 
 pygame.display.set_caption('StickFight | Loading')
 
+1 == mixer.music.load('BGMPotentialKav.wav')
+mixer.music.play(1)
 logo = pygame.image.load('logo.png')
 pygame.display.set_icon(logo)
 
+
 clip = VideoFileClip('Loading.mp4')
-clip.preview()
+while True:
+    clip.preview()
 
 
 
